b660db67ef5a0327bab652c2e5d30e94ca677a8b
andersbandt <andersbandt@yahoo.com>
2021-03-25 16:23:50 -0500

    On branch main  Changes to be committed:

--- nick.py
+++ nick.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[3]:
-
 import requests, json, os
 from bs4 import BeautifulSoup
-
+import sqlite3
+import datetime
 
 url = "https://services.recwell.wisc.edu/FacilityOccupancy"
 resp = requests.get(url)
@@ -13,15 +12,41 @@ resp.raise_for_status()
 
 doc = BeautifulSoup(resp.text, "html.parser")
 
-
 strongs = doc.find_all("strong")
+
+value = 0
+
 for i in strongs:
-    num = i.get_text().replace("%", "")
-    try:
-        num = int(num)
-        if num<=100:
-            value = num
-            break
-    except:
-        pass
-    
+	num = i.get_text().replace("%", "")
+	try:
+		num = int(num)
+		if num <= 100:
+			value = num
+			break
+	except:
+		pass
+
+
+# get the date and time the recording was taken at
+date = datetime.datetime.now()
+
+
+# display some output to the console
+print("The current occupancy is ")
+print(value)
+print("Attempting to connect to occupancy database")
+
+# insert the value into the table
+conn = sqlite3.connect('data.db')
+conn.execute("INSERT INTO OCCUPANCY (DATETIME, OCCUPANCY) \
+		VALUES (?, ?)",
+		(date, value))
+
+conn.commit()
+
+print("Occupancy level saved successfuly")
+
+conn.close()
+
+
+
